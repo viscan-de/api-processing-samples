2069088f4c769abec575af9a4e601cd7a7f4cbfb
PatrickVigramer <patrick.kranzinger@vigram.com>
2024-06-07 02:01:24 +0200

    Progress added to curl command.

--- shell/fileUpload/upload.sh
+++ shell/fileUpload/upload.sh
@@ -4,4 +4,4 @@ FILE=$(basename $1)
 URL="$2$FILE?$3"
 echo "$URL"
 
-curl --request PUT --header "x-ms-blob-type: BlockBlob" --data-binary "@$1" $URL
+curl --progress-bar --verbose --request PUT --header "x-ms-blob-type: BlockBlob" --data-binary "@$1" $URL | tee -a "upload.log" ; test ${PIPESTATUS[0]} -eq 0
